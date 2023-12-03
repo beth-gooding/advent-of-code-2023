5d46c271b17e7355399899f86a5d1c52e0fbe4ba
beth-gooding <beth.gooding@hotmail.com>
2023-12-03 19:48:43 +0000

    Day 03: simplifies solution to part 1 - applies Math.min and Math.max logic to the double for loop, so it accounts for all cases

--- problems/day03/solution.ts
+++ problems/day03/solution.ts
@@ -22,7 +22,6 @@ export const dayThreeSolution = async () : Promise<number[]> => {
     }
 
     // PART 1 
-
     for (let rowNumber: number = 0; rowNumber < engineSchematicMatrix.length; rowNumber++) {
         let row : string = engineSchematicMatrix[rowNumber];
         for (let columnNumber: number = 0; columnNumber < row.length; columnNumber++) {
@@ -38,125 +37,21 @@ export const dayThreeSolution = async () : Promise<number[]> => {
                     columnNumber++;
                 }
 
-                // if not on the outside of the grid, check the whole way around the potential engine part number
-                if ((startingCoordinates[0] !== 0) && (startingCoordinates[0] !== engineSchematicMatrix.length -1) && (startingCoordinates[1] !== 0) && (startingCoordinates[1] !== row.length - 1)) {
-                    for (let i : number = startingCoordinates[0] - 1; i <= startingCoordinates[0] + 1; i++) {
-                        for (let j : number = startingCoordinates[1] - 1; j <= startingCoordinates[1] + potentialEngineNumberToAdd.length; j++) {
-                            if (checkIfSymbol(engineSchematicMatrix[i].charAt(j))) {
-                                sumEnginePartNumbers += Number(potentialEngineNumberToAdd);
-                                break;
-                            }
+                // use logic for the bounds to capture all cases in one double for loop
+                for (let i : number = Math.max(startingCoordinates[0] - 1,0); i <= Math.min(startingCoordinates[0] + 1, engineSchematicMatrix.length - 1); i++) {
+                    for (let j : number = Math.max(startingCoordinates[1] - 1, 0); j <= Math.min(startingCoordinates[1] + potentialEngineNumberToAdd.length, row.length - 1); j++) {
+                        if (checkIfSymbol(engineSchematicMatrix[i].charAt(j))) {
+                            sumEnginePartNumbers += Number(potentialEngineNumberToAdd);
+                            break;
                         }
                     }
-                    continue;
-                }
-
-                // if you're in the top left corner, only check below and to the right
-                if (startingCoordinates[0] === 0 && startingCoordinates[1] === 0){
-                    for (let i : number = 0; i <= 1; i++) {
-                        for (let j : number = 0; j <= potentialEngineNumberToAdd.length; j++) {
-                            if (checkIfSymbol(engineSchematicMatrix[i].charAt(j))) {
-                                sumEnginePartNumbers += Number(potentialEngineNumberToAdd);
-                                break;
-                            }
-                        }
-                    }
-                    continue;
-                }
-
-                // if you're in the top right corner, only check below and to the left
-                if (startingCoordinates[0] === 0 && (startingCoordinates[1] + potentialEngineNumberToAdd.length) === row.length){
-                    for (let i : number = startingCoordinates[0]; i <= 1; i++) {
-                        for (let j : number = startingCoordinates[1] - 1; j <= startingCoordinates[1] + potentialEngineNumberToAdd.length - 1; j++) {
-                            if (checkIfSymbol(engineSchematicMatrix[i].charAt(j))) {
-                                sumEnginePartNumbers += Number(potentialEngineNumberToAdd);
-                                break;
-                            }
-                        }
-                    }
-                    continue;
-                }
-
-                // if you're in the bottom left corner, only check above and to the right
-                if (startingCoordinates[0] === engineSchematicMatrix.length - 1 && startingCoordinates[1] === 0){
-                    for (let i : number = startingCoordinates[0] - 1; i <= startingCoordinates[0]; i++) {
-                        for (let j : number = startingCoordinates[1]; j <= startingCoordinates[1] + potentialEngineNumberToAdd.length; j++) {
-                            if (checkIfSymbol(engineSchematicMatrix[i].charAt(j))) {
-                                sumEnginePartNumbers += Number(potentialEngineNumberToAdd);
-                                break;
-                            }
-                        }
-                    }
-                    continue;
-                }
-
-                // if you're in the bottom right corner, only check above and to the left
-                if (startingCoordinates[0] === engineSchematicMatrix.length - 1 && (startingCoordinates[1] + potentialEngineNumberToAdd.length) === row.length){
-                    for (let i : number = startingCoordinates[0] - 1; i <= startingCoordinates[0]; i++) {
-                        for (let j : number = startingCoordinates[1] - 1; j <= startingCoordinates[1] + potentialEngineNumberToAdd.length - 1; j++) {
-                            if (checkIfSymbol(engineSchematicMatrix[i].charAt(j))) {
-                                sumEnginePartNumbers += Number(potentialEngineNumberToAdd);
-                                break;
-                            }
-                        }
-                    }
-                    continue;
-                }
-
-                // if you're in the first row, only check below, to the right, and to the left
-                if (startingCoordinates[0] === 0){
-                    for (let i : number = 0; i <= 1; i++) {
-                        for (let j : number = startingCoordinates[1] - 1; j <= startingCoordinates[1] + potentialEngineNumberToAdd.length; j++) {
-                            if (checkIfSymbol(engineSchematicMatrix[i].charAt(j))) {
-                                sumEnginePartNumbers += Number(potentialEngineNumberToAdd);
-                                break;
-                            }
-                        }
-                    }
-                    continue;
-                }
-
-                // if you're in the last row, only check above, to the right, and to the left
-                if (startingCoordinates[0] === engineSchematicMatrix.length - 1){
-                    for (let i : number = startingCoordinates[0] - 1; i <= startingCoordinates[0]; i++) {
-                        for (let j : number = startingCoordinates[1] - 1; j <= startingCoordinates[1] + potentialEngineNumberToAdd.length; j++) {
-                            if (checkIfSymbol(engineSchematicMatrix[i].charAt(j))) {
-                                sumEnginePartNumbers += Number(potentialEngineNumberToAdd);
-                                break;
-                            }
-                        }
-                    }
-                    continue;
-                } 
-                
-                // if you're starting in the first column, only check above, below, and to the right
-                if (startingCoordinates[1] === 0){
-                    for (let i : number = startingCoordinates[0] - 1; i <= startingCoordinates[0] + 1; i++) {
-                        for (let j : number = 0; j <= potentialEngineNumberToAdd.length; j++) {
-                            if (checkIfSymbol(engineSchematicMatrix[i].charAt(j))) {
-                                sumEnginePartNumbers += Number(potentialEngineNumberToAdd);
-                                break;
-                            }
-                        }
-                    }
-                    continue;
-                } 
-
-                // if you're in the last column, only check above, below, and to the left
-                if (startingCoordinates[1] === row.length - 1){
-                    for (let i : number = startingCoordinates[0] - 1; i <= startingCoordinates[0] + 1; i++) {
-                        for (let j : number = startingCoordinates[1] - 1; j <= startingCoordinates[1]; j++) {
-                            if (checkIfSymbol(engineSchematicMatrix[i].charAt(j))) {
-                                sumEnginePartNumbers += Number(potentialEngineNumberToAdd);
-                                break;
-                            }
-                        }
-                    }
-                    continue;
-                }                
+                }        
             }
         }
-}
+    }
+
+    // If something is a part number, write a function to check if the symbol is a star, and if it is store the coordinates of the star, or a count per coordinate to see if a coordinate appears more than once
+    // Would need to store co-ordinate and part number, so that part numbers can be multiplied together later
 
     return [sumEnginePartNumbers, 2];
 }
