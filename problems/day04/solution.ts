3060d94d38eba79d4687f99f24121f59e1378d79
beth-gooding <beth.gooding@hotmail.com>
2023-12-04 19:09:06 +0000

    Day 04: adds working solution to part 1

--- problems/day04/solution.ts
+++ problems/day04/solution.ts
@@ -8,15 +8,29 @@ export const dayFourSolution = async () : Promise<number[]> => {
         input : f.createReadStream(inputFile)
     });
 
-
+    let totalPointsWorth : number = 0;
     for await (let scratchCard of scratchCardInterface) {
 
         // PART 1
+        let amountOfWins: number = 0;
 
+        let scratchCardParts : string[] = scratchCard.split(/:|\|/g);
+        let scratchCardWinningNumbers : string[] = scratchCardParts[1].split(" ").filter((s: string) => s !== "");
+        let scratchCardActualNumbers: string[] = scratchCardParts[2].split(" ").filter((s: string) => s !== "");
+        
+        for (let winningNumber of scratchCardWinningNumbers) {
+            if (scratchCardActualNumbers.includes(winningNumber)) {
+                amountOfWins++;
+            }
+        }
+
+        amountOfWins > 0 
+            ? totalPointsWorth += Math.pow(2, amountOfWins - 1) 
+            : null;
 
         // PART 2
 
     }
 
-    return [1, 2];
+    return [totalPointsWorth, 2];
 }
