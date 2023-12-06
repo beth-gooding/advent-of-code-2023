29fc69fe2c13af16dd8984d76e9337c93475a546
beth-gooding <beth.gooding@hotmail.com>
2023-12-06 22:01:19 +0000

    Day 06: adds working solution for part 1

--- problems/day06/solution.ts
+++ problems/day06/solution.ts
@@ -3,20 +3,47 @@ import * as readline from 'node:readline/promises';
 
 const inputFile = './problems/day06/input.txt';
 
+const quadraticEquationSqrt  = (a : number, b : number, c : number) : number => {
+    return Math.sqrt((Math.pow(b, 2) - (4 * a * c)));
+}
+
 export const daySixSolution = async () : Promise<number[]> => {
-    let inputInterface : readline.Interface = readline.createInterface({
+    let boatRaceInterface : readline.Interface = readline.createInterface({
         input : f.createReadStream(inputFile)
     });
 
-
-    for await (let line of inputInterface) {
-
-        // PART 1
-
-
-        // PART 2
-
+    let raceArray : string[][] = [];
+    for await (let line of boatRaceInterface) {
+        // SET UP
+        raceArray.push(line.split(" ").filter((s: string) => s !== ""));
     }
 
-    return [1, 2];
+    let productOfNumberOfRaceWinningScenarios = 1;
+    // PART 1
+    for (let raceNumber: number = 1; raceNumber < raceArray[0].length; raceNumber++) {
+        let time : number = Number(raceArray[0][raceNumber]);
+        let recordDistance : number = Number(raceArray[1][raceNumber]);
+
+        let shortestTime : number = (time - quadraticEquationSqrt(1, time, recordDistance))/2;
+        let longestTime : number = (time + quadraticEquationSqrt(1, time, recordDistance))/2;
+
+        if (shortestTime % 1 === 0) {
+            shortestTime += 1;
+        } else {
+            shortestTime = Math.ceil(shortestTime);
+        }
+
+        if (longestTime % 1 === 0) {
+            longestTime -= 1;
+        } else {
+            longestTime = Math.floor(longestTime);
+        }
+
+        let numberOfWaysToBeatRecord = longestTime - shortestTime + 1; 
+        productOfNumberOfRaceWinningScenarios *= numberOfWaysToBeatRecord;
+    }
+
+    // PART 2
+
+    return [productOfNumberOfRaceWinningScenarios, 2];
 }
