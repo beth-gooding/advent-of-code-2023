76d0bb955cb0317bc64faa9da4db79587b54a0fd
beth-gooding <beth.gooding@hotmail.com>
2023-12-06 22:11:05 +0000

    Day 06: adds working solution for part 2 - refactors code so that answers to both parts can be returned

--- problems/day06/solution.ts
+++ problems/day06/solution.ts
@@ -7,23 +7,7 @@ const quadraticEquationSqrt  = (a : number, b : number, c : number) : number => 
     return Math.sqrt((Math.pow(b, 2) - (4 * a * c)));
 }
 
-export const daySixSolution = async () : Promise<number[]> => {
-    let boatRaceInterface : readline.Interface = readline.createInterface({
-        input : f.createReadStream(inputFile)
-    });
-
-    let raceArray : string[][] = [];
-    for await (let line of boatRaceInterface) {
-        // SET UP
-        raceArray.push(line.split(" ").filter((s: string) => s !== ""));
-    }
-
-    let productOfNumberOfRaceWinningScenarios = 1;
-    // PART 1
-    for (let raceNumber: number = 1; raceNumber < raceArray[0].length; raceNumber++) {
-        let time : number = Number(raceArray[0][raceNumber]);
-        let recordDistance : number = Number(raceArray[1][raceNumber]);
-
+const calculateNumberOfWinningWays = (time : number, recordDistance : number) : number => {
         let shortestTime : number = (time - quadraticEquationSqrt(1, time, recordDistance))/2;
         let longestTime : number = (time + quadraticEquationSqrt(1, time, recordDistance))/2;
 
@@ -39,11 +23,34 @@ export const daySixSolution = async () : Promise<number[]> => {
             longestTime = Math.floor(longestTime);
         }
 
-        let numberOfWaysToBeatRecord = longestTime - shortestTime + 1; 
+        return longestTime - shortestTime + 1; 
+}
+
+export const daySixSolution = async () : Promise<number[]> => {
+    let boatRaceInterface : readline.Interface = readline.createInterface({
+        input : f.createReadStream(inputFile)
+    });
+
+    let raceArray : string[][] = [];
+    for await (let line of boatRaceInterface) {
+        // SET UP
+        raceArray.push(line.split(" ").filter((s: string) => s !== ""));
+    }
+
+    let productOfNumberOfRaceWinningScenarios : number = 1;
+    let actualRaceNumberOfWinningScenarios : number = 0;
+    // PART 1
+    for (let raceNumber: number = 1; raceNumber < raceArray[0].length - 1; raceNumber++) {
+        let time : number = Number(raceArray[0][raceNumber]);
+        let recordDistance : number = Number(raceArray[1][raceNumber]);
+        let numberOfWaysToBeatRecord = calculateNumberOfWinningWays(time, recordDistance);
         productOfNumberOfRaceWinningScenarios *= numberOfWaysToBeatRecord;
     }
 
     // PART 2
+    let actualRaceTime : number = Number(raceArray[0][5]);
+    let actualRecordDistance : number = Number(raceArray[1][5]);
+    actualRaceNumberOfWinningScenarios += calculateNumberOfWinningWays(actualRaceTime, actualRecordDistance);
 
-    return [productOfNumberOfRaceWinningScenarios, 2];
+    return [productOfNumberOfRaceWinningScenarios, actualRaceNumberOfWinningScenarios];
 }
