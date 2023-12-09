7dafe75c0af5f683d6e4a4927aabc6577a76cf98
beth-gooding <beth.gooding@hotmail.com>
2023-12-09 11:46:17 +0000

    Day09: Adds working solution for part 1

--- problems/day09/solution.ts
+++ problems/day09/solution.ts
@@ -3,20 +3,44 @@ import * as readline from 'node:readline/promises';
 
 const inputFile = './problems/day09/input.txt';
 
+const gapFinder = (inputArray : number[]) : number[] => {
+    let outputArray : number[] = [];
+    for (let i : number = 1; i < inputArray.length; i++) {
+        outputArray.push(inputArray[i] - inputArray[i - 1]);
+    }
+    return outputArray;
+}
+
+
 export const dayNineSolution = async () : Promise<number[]> => {
-    let inputInterface : readline.Interface = readline.createInterface({
+    let oasisDataInterface : readline.Interface = readline.createInterface({
         input : f.createReadStream(inputFile)
     });
 
-
-    for await (let line of inputInterface) {
+    let sumOfExtrapolatedValues : number = 0;
+    for await (let dataSequence of oasisDataInterface) {
 
         // PART 1
+        let dataArray : number[][] =  [];
+        dataArray.push(dataSequence.split(" ").map((a : string) => {return Number(a)}));
 
+        let lastRound : number = 0;
+        let notAllGapsZero : boolean = true;
+        while(notAllGapsZero) {
+            dataArray.push(gapFinder(dataArray[lastRound]));
+            lastRound++;
+            if (!dataArray[lastRound].some((gap : number) => gap !== 0)) {notAllGapsZero = false;}
+        }
 
+        dataArray[lastRound].push(0);
+        for (let index : number = lastRound; index > 0; index--) {
+            dataArray[index - 1].push(dataArray[index - 1].slice(-1)[0] + dataArray[index].slice(-1)[0])
+        }
+
+        sumOfExtrapolatedValues += dataArray[0].slice(-1)[0];
         // PART 2
 
     }
 
-    return [1, 2];
+    return [sumOfExtrapolatedValues, 2];
 }
