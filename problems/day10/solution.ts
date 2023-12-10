648bfe663b60d765885fa4590d24131561d1ec3e
beth-gooding <beth.gooding@hotmail.com>
2023-12-10 12:07:55 +0000

    Day10: WIP part 1 - works for example solution - gets stuck at a certain coordinate for real data in one direction

--- problems/day10/solution.ts
+++ problems/day10/solution.ts
@@ -51,6 +51,20 @@ const nextDirection = (currentDirection : string, pipeTravellingThrough: string)
     }
 }
 
+const newCoordinatesCalculator = (currentCoordinates : number[], directionOfTravel: string) : number[] => {
+    switch (directionOfTravel) {
+        case ("N"):
+            return [currentCoordinates[0] - 1, currentCoordinates[1]];
+        case ("E"):
+            return [currentCoordinates[0], currentCoordinates[1] + 1];
+        case ("S"):
+            return [currentCoordinates[0] + 1, currentCoordinates[1]];
+        case ("W"):
+            return [currentCoordinates[0], currentCoordinates[1] - 1];
+        default:
+            return currentCoordinates;
+    }
+}
 
 export const dayTenSolution = async () : Promise<number[]> => {
     let inputInterface : readline.Interface = readline.createInterface({
@@ -72,10 +86,19 @@ export const dayTenSolution = async () : Promise<number[]> => {
     // PART 1
     // I think S has to connect to the North and the East, because South and West pipes miss it (have a corner) - 
     // code below matches that hypothesis
-    console.log("N", checkIfPipeCanJoin("N", pipeMap[sLocation[0] - 1][sLocation[1]]), pipeMap[sLocation[0] - 1][sLocation[1]]);
-    console.log("E", checkIfPipeCanJoin("E", pipeMap[sLocation[0]][sLocation[1] + 1]), pipeMap[sLocation[0]][sLocation[1] + 1]);
-    console.log("S", checkIfPipeCanJoin("S", "F"), pipeMap[sLocation[0] + 1][sLocation[1]]);
-    console.log("W", checkIfPipeCanJoin("W", pipeMap[sLocation[0]][sLocation[1] - 1]), pipeMap[sLocation[0]][sLocation[1] - 1]);
+    let potentialStartPoints : string[][] = [
+        ["N", pipeMap[sLocation[0] - 1][sLocation[1]]], 
+        ["E", pipeMap[sLocation[0]][sLocation[1] + 1]], 
+        ["S", pipeMap[sLocation[0] + 1][sLocation[1]]], 
+        ["W", pipeMap[sLocation[0]][sLocation[1] - 1]]
+    ]
+
+    let potentialNextCoordinates : number[][] = [
+        [sLocation[0] - 1, sLocation[1]], 
+        [sLocation[0], sLocation[1] + 1], 
+        [sLocation[0] + 1, sLocation[1]], 
+        [sLocation[0], sLocation[1] - 1]
+    ];
 
     console.log("Should say N: ", nextDirection("N", pipeMap[sLocation[0] - 1][sLocation[1]]));
     console.log("Should say S: ", nextDirection("E", pipeMap[sLocation[0]][sLocation[1] + 1]));
@@ -83,7 +106,48 @@ export const dayTenSolution = async () : Promise<number[]> => {
     // So - have a function to check which pipes can join to current pipe, and one to calculate the direction of the next pipe.
     // Now just need a smart way to step through the algorithm, and record how many steps have been taken
     // Probably shouldn't assume the loop has an even number of steps - so both directions might not hit the same index at the same time.
+    // record step number and index - when same index is in both lists, stop, because you've covered the whole loop.
+    // Then take the highest index of the location of that in both arrays
+
+    let directionOneSteps : number[][] = [sLocation];
+    let directionOneCompass: string[] = [];
+
+    let directionTwoSteps : number[][] = [sLocation];
+    let directionTwoCompass: string[] = [];
+    
+    for (let i : number = 0; i < potentialStartPoints.length; i++) {
+
+        if (checkIfPipeCanJoin(potentialStartPoints[i][0], potentialStartPoints[i][1])) {
+            if (directionOneSteps.length === 1) { 
+                directionOneSteps.push(potentialNextCoordinates[i]);
+                directionOneCompass.push(potentialStartPoints[i][0]);
+            } else {
+                directionTwoSteps.push(potentialNextCoordinates[i]);
+                directionTwoCompass.push(potentialStartPoints[i][0]);
+            }
+        }
+    }
+
+    while (true) {
+        let currentDirectionOneStep : string = directionOneCompass.slice(-1)[0];
+        let nextCoordinatesDirectionOne : number[] = newCoordinatesCalculator(directionOneSteps.slice(-1)[0], currentDirectionOneStep);
+        directionOneSteps.push(nextCoordinatesDirectionOne);
+        directionOneCompass.push(nextDirection(currentDirectionOneStep, pipeMap[nextCoordinatesDirectionOne[0]][nextCoordinatesDirectionOne[1]]));
+
+        let currentDirectionTwoStep : string = directionTwoCompass.slice(-1)[0];
+        let nextCoordinatesDirectionTwo : number[] = newCoordinatesCalculator(directionTwoSteps.slice(-1)[0], currentDirectionTwoStep);
+        directionTwoSteps.push(nextCoordinatesDirectionTwo);
+        directionTwoCompass.push(nextDirection(currentDirectionTwoStep, pipeMap[nextCoordinatesDirectionTwo[0]][nextCoordinatesDirectionTwo[1]]));
+
+        if ((directionOneSteps.slice(-1)[0][0] === directionTwoSteps.slice(-1)[0][0]) 
+            && (directionOneSteps.slice(-1)[0][1] === directionTwoSteps.slice(-1)[0][1])
+        ) {
+            break;
+        }
+    }
+    console.log(directionOneSteps.slice(-1), directionTwoSteps)
+    let numberOfSteps = directionTwoSteps.length - 1;
     // PART 2
 
-    return [1, 2];
+    return [numberOfSteps, 2];
 }
