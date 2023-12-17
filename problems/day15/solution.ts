a2009464c2a78755c0c0ef92edb1a7ceda5b20a3
beth-gooding <beth.gooding@hotmail.com>
2023-12-17 18:13:08 +0000

    Day15: WIP Part 2 solution - working with example input to calculate the focussing power

--- problems/day15/solution.ts
+++ problems/day15/solution.ts
@@ -25,14 +25,57 @@ export const dayFifteenSolution = async () : Promise<number[]> => {
         initialisationInput = line.split(",");
     }
 
-    // PART 1
+    // PART 1 and 2
     let totalHashResults : number = 0;
+    let boxHashMap : {[key : number] : string[]} = {};
     for (let input of initialisationInput) {
+        // Part 1
         totalHashResults += hashValue(input);
+
+        // Part 2
+        let boxNumber : number = hashValue(input.split(/-|=/).join("").replace(/[0-9]/, ""));
+        let boxLabelAndLens : string = input.split(/-|=/).join("");
+
+        if (input.includes("-")) {
+            if (boxHashMap[boxNumber]?.filter((s: string) => s.includes(boxLabelAndLens.replace(/[0-9]/, ""))).length > 0) {
+                let actualEntry = boxHashMap[boxNumber].filter((s: string) => s.includes(boxLabelAndLens.replace(/[0-9]/, "")))[0]
+                let indexOfLabel : number = boxHashMap[boxNumber].indexOf(actualEntry);
+                boxHashMap[boxNumber] = [...boxHashMap[boxNumber].slice(0, indexOfLabel), ...boxHashMap[boxNumber].slice(indexOfLabel + 1)];
+                continue;
+            }
+        }
+
+        if (input.includes("=")) {
+            if (boxHashMap[boxNumber]?.filter((s: string) => s.includes(boxLabelAndLens.replace(/[0-9]/, ""))).length > 0) {
+                let actualEntry = boxHashMap[boxNumber].filter((s: string) => s.includes(boxLabelAndLens.replace(/[0-9]/, "")))[0]
+                let indexOfLabel : number = boxHashMap[boxNumber].indexOf(actualEntry);
+                boxHashMap[boxNumber] = [...boxHashMap[boxNumber].slice(0, indexOfLabel), boxLabelAndLens, ...boxHashMap[boxNumber].slice(indexOfLabel + 1)];
+                continue;
+            } 
+            
+            if (boxHashMap[boxNumber]) {
+                boxHashMap[boxNumber] = [...boxHashMap[boxNumber], boxLabelAndLens];
+                continue;
+            }
+
+            if (boxHashMap[boxNumber] === undefined) {
+                boxHashMap[boxNumber] = [boxLabelAndLens];
+            }
+        }
+    }
+
+    console.log(boxHashMap)
+
+    let focussingPower = 0;
+
+    for (let box of Object.keys(boxHashMap)) {
+        let numberBox : number = Number(box);
+        for (let i :  number = 0; i < boxHashMap[numberBox].length; i++) {
+            console.log("hello", boxHashMap[numberBox][i].replace(/[a-zA-Z]/g, ""))
+            focussingPower += ((numberBox + 1) * (i) * (Number(boxHashMap[numberBox][i].replace(/[a-zA-Z]/g, ""))));
+        }
     }
 
 
-    // PART 2
-
-    return [totalHashResults, 2];
+    return [totalHashResults, focussingPower];
 }
