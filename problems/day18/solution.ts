6ddcff461442362ab896eeec982638e995d7874f
beth-gooding <beth.gooding@hotmail.com>
2023-12-22 15:11:38 +0000

    Day18: WIP trying shoelace theorem to get area in test case

--- problems/day18/solution.ts
+++ problems/day18/solution.ts
@@ -41,24 +41,43 @@ export const dayEighteenSolution = async () : Promise<number[]> => {
         input : f.createReadStream(inputFile)
     });
 
-    let instructions : string[] = [];
+    let instructions : string[][] = [];
     for await (let instruction of digInstructionsInterface) {
-        instructions.push(instruction);
+        instructions.push(instruction.split(" "));
     }
 
     // PART 1
-    console.log(instructions)
     let trench : number[][] = [[0, 0]];
+    let corners : number[][] = [];
 
-    for (let instruction of instructions) {
-        let instructionPieces : string[] = instruction.split(" ");
+    for (let instructionIndex : number = 0; instructionIndex < instructions.length; instructionIndex++) {
+
+        trench.push(...nextTrenchSquares(instructions[instructionIndex][0], Number(instructions[instructionIndex][1]), trench.slice(-1)[0]));
         
-        trench.push(...nextTrenchSquares(instructionPieces[0], Number(instructionPieces[1]), trench.slice(-1)[0]));
+        if (instructions[instructionIndex][0] !== instructions[(instructionIndex + 1) % instructions.length][0]) {
+            corners.push(trench.at(-1));
+        }
     }
 
-    console.log("The volume of the trench is", trench.length - 1);
+    let shoelaceTheoremSumPositive : number = 0;
+    let shoelaceTheoremSumNegative : number = 0;
+
+    // find the squares inside the border trench
+    for (let coordinateNumber : number = 0; coordinateNumber < corners.length; coordinateNumber++) {
+        shoelaceTheoremSumPositive += (corners[coordinateNumber][0] * corners[(coordinateNumber + 1) % corners.length][1]); 
+        shoelaceTheoremSumNegative += (corners[coordinateNumber][1] * corners[(coordinateNumber + 1) % corners.length][0]);
+
+        console.log(shoelaceTheoremSumPositive, shoelaceTheoremSumNegative)
+    }
+
+    console.log(corners)
+
+    let lagoonArea = Math.abs(shoelaceTheoremSumPositive - shoelaceTheoremSumNegative) / 2;
+
+    // too low: Math.abs(shoelaceTheoremSum / 2) = 
+    // too high: Math.abs(shoelaceTheoremSum / 2) + trench.length - 1;
 
     // PART 2
 
-    return [1, 2];
+    return [lagoonArea, 2];
 }
